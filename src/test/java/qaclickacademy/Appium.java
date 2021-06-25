f51d8dfa105b43ee0e3c278772fa884ec861b46b
hamza <hamzacherfaoui32@yahoo.com>
2021-06-25 00:01:51 -0400

    this commit from Asian developer

--- src/test/java/qaclickacademy/Appium.java
+++ src/test/java/qaclickacademy/Appium.java
@@ -3,15 +3,18 @@ package qaclickacademy;
 import org.testng.annotations.Test;
 
 public class Appium {
-	@Test
-}
+	
+@Test
 
 	public void nativeApp() {
 		System.out.println("Native app");
+		System.out.println("Native app1");
 		
 	}
 	@Test
 	public void IOSApps() {
 		System.out.println("IOS APP");
+		System.out.println("IOS APP2");
 		
+	}
 	}
